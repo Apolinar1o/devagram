e7a9a4764833ad778302154f1db4a6f123b9915a
Antônio Apolinário Gonzaga Neto <a.apolinario.neto@gmail.com>
2023-04-07 15:42:53 -0300

    .

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,14 +1,22 @@
     import type {NextApiRequest, NextApiResponse} from "next"
     import {conectarMongoDB} from "../../middlewares/conectaBancoDeDados" 
     import type {RespostaPadramsg} from "../../types/respostaPadraoMsg"
+    import type {RespostaPadramsg} from "../../types/loginResposta"
     import md5 from "md5"
-import { usuarioModel } from "@/models/usuarioModel"
+    import { usuarioModel } from "@/models/usuarioModel"
     export {usuarioModel} from "../../models/usuarioModel"
+    import jwt from "jsonwebtoken"
     
     const endpointLogin =  async(   
         req: NextApiRequest,
-        res: NextApiResponse<RespostaPadramsg>
+        res: NextApiResponse<RespostaPadramsg | any>
     ) => {
+
+        const {MINHA_CHAVE_JWT} = process.env
+        if(!MINHA_CHAVE_JWT) {
+           return res.status(500).json({erro: "env jwt não informada"})
+        }
+
         if(req.method == "POST") {
             const {login, senha} = req.body
 
@@ -16,7 +24,13 @@ import { usuarioModel } from "@/models/usuarioModel"
             if(usuariosEncontrados && usuariosEncontrados.length > 0) {
                 
                 const usuarioLogado = usuariosEncontrados[0]
-                return res.status(200).json({msg : `Usuario ${usuarioLogado.nome} autenticado com sucesso`})
+
+                const token = jwt.sign({_id: usuarioLogado._id}, MINHA_CHAVE_JWT)
+
+                return res.status(200).json({
+                    nome: usuarioLogado.nome,
+                     email: usuarioLogado.email,
+                     token})
 
             } else {
                 return res.status(405).json({erro: "Usuario ou senha não encontrados"})
